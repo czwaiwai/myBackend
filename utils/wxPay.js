4a8282115f24d5ed652935e9e69eb024a6d92b70
czwaiwai <278178596@qq.com>
2018-04-25 23:37:14 +0800

    fix run error

--- utils/wxPay.js
+++ utils/wxPay.js
@@ -1,11 +1,11 @@
 var config = require('../config'); //配置文件 appid 等信息
-var Q = require("q");
+// var Q = require("q");
 var request = require("request");
 var crypto = require('crypto');
-var ejs = require('ejs');
-var fs = require('fs');
+// var ejs = require('ejs');
+// var fs = require('fs');
 var key = "此处为申请微信支付的API密码";
-var messageTpl = fs.readFileSync(__dirname + '/message.ejs', 'utf-8');
+// var messageTpl = fs.readFileSync(__dirname + '/message.ejs', 'utf-8');
 
 var WxPay = {
 	getXMLNodeValue: function(node_name, xml) {
@@ -29,11 +29,11 @@ var WxPay = {
 		return string;
 	},
 	
-	paysignjs: function(appid, nonceStr, package, signType, timeStamp) {
+	paysignjs: function(appid, nonceStr, wxPackage, signType, timeStamp) {
 		var ret = {
 			appId: appid,
 			nonceStr: nonceStr,
-			package: package,
+			package: wxPackage,
 			signType: signType,
 			timeStamp: timeStamp
 		};
@@ -76,52 +76,52 @@ var WxPay = {
 	
 	// 此处的attach不能为空值 否则微信提示签名错误
 	order: function(attach, body, mch_id, openid, bookingNo, total_fee, notify_url) {
-		var deferred = Q.defer();
-		var appid = config.member_config.appid;
-		var nonce_str = this.createNonceStr();
-		var timeStamp = this.createTimeStamp();
-		var url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
-		var formData = "<xml>";
-		formData += "<appid>" + appid + "</appid>"; //appid
-		formData += "<attach>" + attach + "</attach>"; //附加数据
-		formData += "<body>" + body + "</body>"; //商品或支付单简要描述
-		formData += "<mch_id>" + mch_id + "</mch_id>"; //商户号
-		formData += "<nonce_str>" + nonce_str + "</nonce_str>"; // 随机字符串，不长于32位
-		formData += "<notify_url>" + notify_url + "</notify_url>"; // 支付成功后微信服务器通过POST请求通知这个地址
-		formData += "<openid>" + openid + "</openid>"; // 扫码支付这个参数不是必须的
-		formData += "<out_trade_no>" + bookingNo + "</out_trade_no>"; //订单号
-		formData += "<spbill_create_ip>61.50.221.43</spbill_create_ip>"; //不是必须的
-		formData += "<total_fee>" + total_fee + "</total_fee>"; //金额
-		formData += "<trade_type>JSAPI</trade_type>"; //NATIVE会返回code_url ，JSAPI不会返回
-		formData += "<sign>" + this.paysignjsapi(appid, attach, body, mch_id, nonce_str, notify_url, openid, bookingNo, '61.50.221.43', total_fee, 'JSAPI') + "</sign>";
-		formData += "</xml>";
-		var self = this;
-		request({
-			url: url,
-			method: 'POST',
-			body: formData
-		}, function(err, response, body) {
-			if (!err && response.statusCode == 200) {
-				console.log(body);
-				var prepay_id = self.getXMLNodeValue('prepay_id', body.toString("utf-8"));
-				var tmp = prepay_id.split('[');
-				var tmp1 = tmp[2].split(']');
-				//签名
-				var _paySignjs = self.paysignjs(appid, nonce_str, 'prepay_id=' + tmp1[0], 'MD5', timeStamp);
-				var args = {
-					appId: appid,
-					timeStamp: timeStamp,
-					nonceStr: nonce_str,
-					signType: "MD5",
-					package: tmp1[0],
-					paySign: _paySignjs
-				};
-				deferred.resolve(args);
-			} else {
-				console.log(body);
-			}
-		});
-		return deferred.promise;
+		return new Promise((resolve,reject) => {
+			var appid = config.member_config.appid;
+			var nonce_str = this.createNonceStr();
+			var timeStamp = this.createTimeStamp();
+			var url = "https://api.mch.weixin.qq.com/pay/unifiedorder";
+			var formData = "<xml>";
+			formData += "<appid>" + appid + "</appid>"; //appid
+			formData += "<attach>" + attach + "</attach>"; //附加数据
+			formData += "<body>" + body + "</body>"; //商品或支付单简要描述
+			formData += "<mch_id>" + mch_id + "</mch_id>"; //商户号
+			formData += "<nonce_str>" + nonce_str + "</nonce_str>"; // 随机字符串，不长于32位
+			formData += "<notify_url>" + notify_url + "</notify_url>"; // 支付成功后微信服务器通过POST请求通知这个地址
+			formData += "<openid>" + openid + "</openid>"; // 扫码支付这个参数不是必须的
+			formData += "<out_trade_no>" + bookingNo + "</out_trade_no>"; //订单号
+			formData += "<spbill_create_ip>61.50.221.43</spbill_create_ip>"; //不是必须的
+			formData += "<total_fee>" + total_fee + "</total_fee>"; //金额
+			formData += "<trade_type>JSAPI</trade_type>"; //NATIVE会返回code_url ，JSAPI不会返回
+			formData += "<sign>" + this.paysignjsapi(appid, attach, body, mch_id, nonce_str, notify_url, openid, bookingNo, '61.50.221.43', total_fee, 'JSAPI') + "</sign>";
+			formData += "</xml>";
+			var self = this;
+			request({
+				url: url,
+				method: 'POST',
+				body: formData
+			}, function(err, response, body) {
+				if (!err && response.statusCode == 200) {
+					console.log(body);
+					var prepay_id = self.getXMLNodeValue('prepay_id', body.toString("utf-8"));
+					var tmp = prepay_id.split('[');
+					var tmp1 = tmp[2].split(']');
+					//签名
+					var _paySignjs = self.paysignjs(appid, nonce_str, 'prepay_id=' + tmp1[0], 'MD5', timeStamp);
+					var args = {
+						appId: appid,
+						timeStamp: timeStamp,
+						nonceStr: nonce_str,
+						signType: "MD5",
+						package: tmp1[0],
+						paySign: _paySignjs
+					};
+					resolve(args);
+				} else {
+					console.log(body);
+				}
+			});
+		})
 	},
 	
 	//支付回调通知
@@ -140,7 +140,7 @@ var WxPay = {
 			};
 		}
 		
-		output = ejs.render(messageTpl, reply);
+		// output = ejs.render(messageTpl, reply);
 		return output;
 	},
 };
