befe1c2352d6050dfc128d231b094bd7a569c185
czwaiwai <278178596@qq.com>
2020-05-28 00:00:57 +0800

    添加链接

--- models/phase.js
+++ models/phase.js
@@ -8,6 +8,9 @@ var PhaseSchema =  new Schema({
   content: {type: String},
   intro: {type: String},
   url: {type: String}, // 扫码用的url
+  lineShow: {type: String}, // all 00 01 02 03 04
+  linkName: {type: String}, // 00|标题名称
+  linkUrl: {type: String},
   status: {type: Number, default: 0}, // 是否有效,
   isDel: {type: Number, default: 0}, // 是否删除
 	create_at: {type: Date, default: Date.now},
